c44196d2b760b978f2afad6525ec751e831f41a7
rysouke1217 <ryosuke121720080711@gmail.com>
2017-04-09 10:07:53 +0000

    本日作業分

--- lstm_model.py
+++ lstm_model.py
@@ -2,50 +2,64 @@
 
 import tensorflow as tf
 
-class Input(object):
-	def __init__(self
-	, data
-	, barch_size = 64
-	, num_steps = 20
-	):
-		self.batch_size = batch_size = config.batch_size
-		self.num_steps = num_steps = config.num_steps
-		self.epoch_size = ((len(data) // batch_size) - 1) // num_steps
-		self.input_data, self.targets = NWModel.ptb_producer(data, batch_size, num_steps, name=name)
-
 class NWModel(object):
-	def __init__(self
-	, batch_size
-	, num_steps
-	, hidden_size
-	, vocab_size
-	, num_layers
-	, input_
-	, keep_prob
-	, is_training
-	):
-		self._input = input_
-		self.batch_size = input_.batch_size
-		self.num_steps = input_.num_steps
-		self.size = hidden_size
-		self.vocab_size = vocab_size
-		self.is_training = is_training #訓練用かどうか
+    def __init__(self
+    , train_input
+    , batch_size
+    , num_steps
+    , hidden_size
+    , vocab_size
+    , num_layers
+    ):
+        self.batch_size = batch_size
+        self.num_steps = num_steps
+        self.size = hidden_size
+        self.vocab_size = vocab_size
+        self.cell = tf.contrib.rnn.MultiRNNCell([tf.contrib.rnn.BasicLSTMCell(hidden_size, forget_bias=0.0, state_is_tuple=True) for _ in range(num_layers)], state_is_tuple=True)
+        self.state = self.cell.zero_state(batch_size, tf.float32)
+        self.input_data, self.targets = self.ptb_producer([1 for _ in range(100000)], batch_size, num_steps)
 
-		cell = tf.contrib.rnn.MultiRNNCell([tf.contrib.rnn.BasicLSTMCell(size, forget_bias=0.0, state_is_tuple=True) for _ in range(config.num_layers)], state_is_tuple=True)
-		self._initial_state = cell.zero_state(batch_size, data_type())
+    def logits(self):
+        outputs = []
+        embedding = tf.get_variable("embedding", [self.vocab_size, self.size], tf.float32)
+        inputs = tf.nn.embedding_lookup(embedding, self.input_data)
+        softmax_w = tf.get_variable("softmax_w", [self.size, self.vocab_size], tf.float32)
+        softmax_b = tf.get_variable("softmax_b", [self.vocab_size], tf.float32)
+        for time_step in range(self.num_steps):
+            if time_step > 0: tf.get_variable_scope().reuse_variables()
+            (cell_output, state) = self.cell(inputs[:, time_step, :], self.state)
+            outputs.append(cell_output)
+        output = tf.reshape(tf.concat(axis=1, values=outputs), [-1, self.size])
+        return tf.matmul(output, softmax_w) + softmax_b
 
-	def loss(self, logits):
-		pass
+
+    def loss(self, logits, targets, weights):
+        loss = tf.contrib.seq2seq.sequence_loss(logits, targets, weights)
+        return tf.reduce_sum(loss) / batch_size
 
 #self.learning_rateを定義すること
-	def training(self, cost):
-		optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(cost)
-		return optimizer
+    def training(self, cost):
+        optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(cost)
+        return optimizer
 
-	@property
-	def input(self):
-		return self._input
+    @property
+    def input(self):
+        return self._input
 
-	@property
-	def initial_state(self):
-		return self._initial_state
+    @property
+    def initial_state(self):
+        return self._initial_state
+
+    def ptb_producer(self, raw_data, batch_size, num_steps, name=None):
+        with tf.name_scope("PTBProducer"):
+            raw_data = tf.convert_to_tensor(raw_data, name="raw_data", dtype=tf.int32)
+            data_len = tf.size(raw_data)
+            batch_len = data_len // batch_size
+            data = tf.reshape(raw_data[0 : batch_size * batch_len],[batch_size, batch_len])
+            epoch_size = (batch_len - 1) // num_steps
+            i = 1
+            x = tf.strided_slice(data, [0, i * num_steps],[batch_size, (i + 1) * num_steps])
+            x.set_shape([batch_size, num_steps])
+            y = tf.strided_slice(data, [0, i * num_steps + 1],[batch_size, (i + 1) * num_steps + 1])
+            y.set_shape([batch_size, num_steps])
+            return x, y
--- main.py
+++ main.py
@@ -1,11 +1,40 @@
 # -*- coding: utf-8 -*-
-
+"""
+- init_scale - 重みの初期尺度
+- learning_rate - 学習率の初期値
+- max_grad_norm - 勾配の最大許容ノルム
+- num_layers - LSTM層の数
+- num_steps - LSTMのアンロールされたステップの数
+- hidden_size - LSTMユニットの数
+- max_epoch - 最初の学習率で訓練されたエポックの数
+- max_max_epoch - トレーニングのエポックの総数
+- keep_prob - ドロップアウト層にウェイトを保持する確率
+- lr_decay - "max_epoch"後の各エポックの学習率の減衰
+- batch_size - バッチサイズ
+"""
 import tensorflow as tf
 import input_reader
 import lstm_model
 
 def main():
-	train_data = input_reader.raw_data("data/ptb.train.txt")
+    with tf.Graph().as_default():
+        train_data = input_reader.raw_data("data/ptb.train.txt")
+
+        m = lstm_model.NWModel(
+            train_data
+            , batch_size = 64
+            , num_steps = 20
+            , hidden_size = 200
+            , vocab_size = 10000
+            , num_layers = 2
+            )
+
+        logits = m.logits()
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            print(sess.run(m.input_data))
+            print(sess.run(logits))
+
 
 if __name__ == '__main__':
-	main()
+    main()
