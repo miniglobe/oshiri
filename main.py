c7c9da894c841634fd30c8472161e368cc0ccd93
miumiu0917 <miumiu917@gmail.com>
2017-04-12 04:26:00 +0000

    updated

--- main.py
+++ main.py
@@ -87,12 +87,12 @@ def create_model(session, forward_only):
       forward_only=forward_only)
 
   ckpt = tf.train.get_checkpoint_state(FLAGS.train_dir)
-  if ckpt and gfile.Exists(ckpt.model_checkpoint_path):
+  if ckpt:
     print("Reading model parameters from %s" % ckpt.model_checkpoint_path)
     model.saver.restore(session, ckpt.model_checkpoint_path)
   else:
     print("Created model with fresh parameters.")
-    session.run(tf.initialize_all_variables())
+    session.run(tf.global_variables_initializer())
   return model
 
 
