2ddd28dd0bf8972d0d804c6c89708a1dc3d8e946
rysouke1217 <ryosuke121720080711@gmail.com>
2017-05-25 08:59:07 +0000

    updated

--- input_reader.py
+++ input_reader.py
@@ -33,7 +33,7 @@ _PAD = b"_PAD"
 _GO = b"_GO"
 _EOS = b"_EOS"
 _UNK = b"_UNK"
-_START_VOCAB = [_MYSELF,_OPPONENT,_PAD, _GO, _EOS, _UNK]
+_START_VOCAB = [_PAD, _GO, _EOS, _UNK]
 
 MYSELF_ID = 0
 OPPONENT_ID = 1
